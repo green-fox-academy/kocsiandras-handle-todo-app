55e17ace7d57bcff100af2c05e0d4e98d6ff5014
kocsiandras <kocsi.andras@yahoo.com>
2020-07-15 20:34:56 +0200

    ignore it

--- src/Todo.java
+++ src/Todo.java
@@ -1,5 +1,43 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Todo {
   public static void main(String[] args) {
-    
+    List<String> content = new ArrayList<>();
+    content.add("Walk the dog");
+    content.add("Buy milk");
+    content.add("Do homework");
+    try {
+      Path filepath = Paths.get("tasks.txt");
+      Files.write(filepath, content);
+    } catch (Exception e) {
+      System.out.println("not working");
+    }
+    if (args.length == 0) {
+      System.out.println(
+          "Command Line Todo application\n" +
+              "=============================\n" +
+              "\n" +
+              "Command line arguments:\n" +
+              "    -l   Lists all the tasks\n" +
+              "    -a   Adds a new task\n" +
+              "    -r   Removes an task\n" +
+              "    -c   Completes an task");
+    } else if (args.length == 1) {
+      if (args[0].equals("-l")) {
+        try {
+          Path filePath = Paths.get("tasks.txt");
+          List<String> lines = Files.readAllLines(filePath);
+          for (int i = 0; i < lines.size(); i++) {
+            System.out.println(i + 1 + " - " + lines.get(i));
+          }
+        } catch (Exception e) {
+          System.out.println("not working");
+        }
+      }
+    }
   }
 }
