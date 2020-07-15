3f9973022441313f6dc5d170f53a524c61942b22
kocsiandras <kocsi.andras@yahoo.com>
2020-07-15 20:42:20 +0200

    Empty list done

--- src/Todo.java
+++ src/Todo.java
@@ -31,6 +31,9 @@ public class Todo {
         try {
           Path filePath = Paths.get("tasks.txt");
           List<String> lines = Files.readAllLines(filePath);
+          if(lines.size()==0){
+            System.out.println("No todos for today! :)");
+          }
           for (int i = 0; i < lines.size(); i++) {
             System.out.println(i + 1 + " - " + lines.get(i));
           }
