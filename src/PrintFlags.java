cd2074f52ec8d20bfee54fa15bd2fc31c1412059
kocsiandras <kocsi.andras@yahoo.com>
2020-07-18 12:50:06 +0200

    print method done

--- src/PrintFlags.java
+++ src/PrintFlags.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
 public class PrintFlags {
+  public void printFlags(){
+    System.out.println(
+        "Command Line Todo application\n" +
+            "=============================\n" +
+            "\n" +
+            "Command line arguments:\n" +
+            "    -l   Lists all the tasks\n" +
+            "    -a   Adds a new task\n" +
+            "    -r   Removes an task\n" +
+            "    -c   Completes an task");
+  }
 }
--- src/Todo.java
+++ src/Todo.java
@@ -6,52 +6,32 @@ import java.util.List;
 
 public class Todo {
   public static void main(String[] args) {
+    AddNewTask addNewTask = new AddNewTask();
+    ListTasks listTasks = new ListTasks();
+    PrintFlags printFlags = new PrintFlags();
+
     List<String> content = new ArrayList<>();
     content.add("Walk the dog");
     content.add("Buy milk");
     content.add("Do homework");
+    Path filepath = Paths.get("tasks.txt");
+
     try {
-      Path filepath = Paths.get("tasks.txt");
+      content = Files.readAllLines(filepath);
+      filepath = Paths.get("tasks.txt");
       Files.write(filepath, content);
     } catch (Exception e) {
       System.out.println("not working");
     }
+
     if (args.length == 0) {
-      System.out.println(
-          "Command Line Todo application\n" +
-              "=============================\n" +
-              "\n" +
-              "Command line arguments:\n" +
-              "    -l   Lists all the tasks\n" +
-              "    -a   Adds a new task\n" +
-              "    -r   Removes an task\n" +
-              "    -c   Completes an task");
-    } else if (args.length == 1) {
-      if (args[0].equals("-l")) {
-        try {
-          Path filePath = Paths.get("tasks.txt");
-          List<String> lines = Files.readAllLines(filePath);
-          if (lines.size() == 0) {
-            System.out.println("No todos for today! :)");
-          }
-          for (int i = 0; i < lines.size(); i++) {
-            System.out.println(i + 1 + " - " + lines.get(i));
-          }
-        } catch (Exception e) {
-          System.out.println("not working");
-        }
-      }
-    } else if (args.length == 2) {
-      if (args[0].equals("-a") && args[1].equals("Feed the monkey")) {
-        content.add("Feed the monkey");
-        try {
-          Path filepath = Paths.get("tasks.txt");
-          Files.write(filepath, content);
-        } catch (Exception e) {
-          System.out.println("not working");
-        }
-      }
+      printFlags.printFlags();
+    } else if (args[0].equals("-l")) {
+      listTasks.listTasks();
+    } else if (args.length == 2 && args[0].equals("-a")) {
+      addNewTask.addNewTask(args[1]);
+    } else if (args.length == 1 && args[0].equals("-a")) {
+      System.out.println("Unable to add: no task provided");
     }
   }
-
 }
