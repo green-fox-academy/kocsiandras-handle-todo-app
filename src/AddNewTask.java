4ba4d536cbe8e280abc263fde7a60d350fa2fe41
kocsiandras <kocsi.andras@yahoo.com>
2020-07-18 12:49:26 +0200

    addnew task done

--- src/AddNewTask.java
+++ src/AddNewTask.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AddNewTask {
+  public void addNewTask(String input) {
+    try {
+      Path filepath = Paths.get("tasks.txt");
+      List<String> lines = Files.readAllLines(filepath);
+      lines.add(input);
+      Files.write(filepath, lines);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
 }
--- src/ListTasks.java
+++ src/ListTasks.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
 public class ListTasks {
+  public void listTasks() {
+    try {
+      Path filePath = Paths.get("tasks.txt");
+      List<String> lines = Files.readAllLines(filePath);
+      if (lines.size() == 0) {
+        System.out.println("No todos for today! :)");
+      }
+      for (int i = 0; i < lines.size(); i++) {
+        System.out.println(i + 1 + " - " + lines.get(i));
+      }
+    } catch (Exception e) {
+      System.out.println("not working");
+    }
+  }
 }
